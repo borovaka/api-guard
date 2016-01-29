2c574d996defbdbd8cd99ad0b661206e45514258
Borislav Anchev <borislav@anchev.eu>
2016-01-29 08:10:49 +0200

    Revert "Add user relation to model"
    This reverts commit 72eab822924f05a6963e63a619832a2300b75a9c.

--- src/Models/ApiKey.php
+++ src/Models/ApiKey.php
@@ -7,9 +7,6 @@ use Chrisbjr\ApiGuard\Repositories\ApiKeyRepository;
 class ApiKey extends ApiKeyRepository
 {
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    //
 
 }
