72eab822924f05a6963e63a619832a2300b75a9c
Borislav Anchev <borislav@anchev.eu>
2016-01-29 07:48:01 +0200

    Add user relation to model

--- src/Models/ApiKey.php
+++ src/Models/ApiKey.php
@@ -7,6 +7,9 @@ use Chrisbjr\ApiGuard\Repositories\ApiKeyRepository;
 class ApiKey extends ApiKeyRepository
 {
 
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
